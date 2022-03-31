885dc7d1e174058fcb460ace2ab5d4f4590e42f1
Manaud <manaud57@gmil.com>
2022-03-31 15:24:11 +0200

    Jauge de température à continuer

--- js/gauge_temperature.js
+++ js/gauge_temperature.js
@@ -1,4 +1,16 @@
 const gaugeElement = document.querySelector(".gauge");
+var A_temperatures = [];
+var GaugeMaxValue = 51;
+var color_result = "#3d7e29";
+var color_min = "#00f2ff";
+var color_medium = "#ff8600";
+var color_maxi = "#ff0000";
+var value = 0;
+
+
+function getRandomArbitrary(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 function setGaugeValue(gauge, value){
     if (value < 0 || value > 1 ){
@@ -9,4 +21,4 @@ function setGaugeValue(gauge, value){
     gauge.querySelector(".gauge__cover").textContent = `${Math.round(value * 100)}`;
 }
 
-setGaugeValue(gaugeElement, 0.3);
+setGaugeValue(gaugeElement, 0.8);
